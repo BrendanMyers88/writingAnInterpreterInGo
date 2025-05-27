8a0a83f4c8c0b78263a60b02ab2e2ffb4c349300
BrendanMyers88 <BrendanMyers88@users.noreply.github.com>
2025-05-27 15:02:20 -0700

    Updates repl.go to use tabs like all other files
    Looks like `repl.go` got missed in the formatting fix. This updates it to
    be in line with the rest of the `*.go` files in this project.

--- repl/repl.go
+++ repl/repl.go
@@ -1,12 +1,12 @@
 package repl
 
 import (
-  "bufio"
-  "fmt"
-  "io"
-  "monkey/evaluator"
-  "monkey/lexer"
-  "monkey/parser"
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/evaluator"
+	"monkey/lexer"
+	"monkey/parser"
 )
 
 const MONKEY_FACE = `            __,__
@@ -25,42 +25,42 @@ const MONKEY_FACE = `            __,__
 const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
-  scanner := bufio.NewScanner(in)
+	scanner := bufio.NewScanner(in)
 
-  for {
-    fmt.Fprintf(out, PROMPT)
-    scanned := scanner.Scan()
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
 
-    if !scanned {
-      return
-    }
+		if !scanned {
+			return
+		}
 
-    line := scanner.Text()
-    l := lexer.New(line)
-    p := parser.New(l)
-    program := p.ParseProgram()
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
 
-    if len(p.Errors()) != 0 {
-      printParserErrors(out, p.Errors())
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
 
-      continue
-    }
+			continue
+		}
 
-    evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program)
 
-    if evaluated != nil {
-      io.WriteString(out, evaluated.Inspect())
-      io.WriteString(out, "\n")
-    }
-  }
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
 }
 
 func printParserErrors(out io.Writer, errors []string) {
-  io.WriteString(out, MONKEY_FACE)
-  io.WriteString(out, "Whoops! we ran into some monkey business here!\n")
-  io.WriteString(out, " parser errors:\n")
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Whoops! we ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
 
-  for _, msg := range errors {
-    io.WriteString(out, "\t"+msg+"\n")
-  }
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
